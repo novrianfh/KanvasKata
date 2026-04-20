95a23ddefee9016e99a1a147c38366c313dd196a
Novrian <novrian.fh@live.com>
2026-04-20 09:05:04 +0700

    index element

--- KanvasKata/Features/Editor/Services/CanvasStateService.cs
+++ KanvasKata/Features/Editor/Services/CanvasStateService.cs
@@ -31,5 +31,37 @@ public class CanvasStateService
         OnChange?.Invoke();
     }
 
+    public void BringForward(CanvasItem item)
+    {
+        var i = _items.IndexOf(item);
+        if (i < _items.Count - 1)
+            (_items[i], _items[i + 1]) = (_items[i + 1], _items[i]);
+        OnChange?.Invoke();
+    }
+
+    public void SendBackward(CanvasItem item)
+    {
+        var i = _items.IndexOf(item);
+        if (i > 0)
+            (_items[i], _items[i - 1]) = (_items[i - 1], _items[i]);
+        OnChange?.Invoke();
+    }
+
+    public void BringToFront(CanvasItem item)
+    {
+        _items.Remove(item);
+        _items.Add(item);
+        OnChange?.Invoke();
+    }
+
+    public void SendToBack(CanvasItem item)
+    {
+        _items.Remove(item);
+        _items.Insert(0, item);
+        OnChange?.Invoke();
+    }
+
+    public int IndexOf(CanvasItem item) => _items.IndexOf(item);
+
     public void NotifyChange() => OnChange?.Invoke();
 }
